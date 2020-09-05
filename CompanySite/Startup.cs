0c86810adaf9fa03415ef6cb36d746cea90086e6
mitrius99 <mitrius99o@gmail.com>
2020-09-05 15:47:47 +0300

    Added View and Controller parts of the project

--- CompanySite/Startup.cs
+++ CompanySite/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,28 +13,32 @@ namespace CompanySite
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //поддержка контроллеров и представлений и установка совместимости с asp.net core 3.0
+            services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Порядок подключения сервисов очень важен!!!
+            //1.Вывод сообщения об ошибках при их наличии
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //2.Настройка маршрутизации
             app.UseRouting();
 
+            //3.Подключениеподдержки файлов (css, html, js)
+            app.UseStaticFiles();
+            
+            //4.Регистация нужных нам маршрутов(endpoints)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
