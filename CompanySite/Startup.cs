b64c1f642cb495da886a233e2033f4d9300e2ba3
mitrius99 <mitrius99o@gmail.com>
2020-09-06 16:47:12 +0300

    Created Config.cs. Connected appsettings.json and Config.cs

--- CompanySite/Startup.cs
+++ CompanySite/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanySite.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,6 +15,8 @@ namespace CompanySite
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
             //поддержка контроллеров и представлений и установка совместимости с asp.net core 3.0
@@ -23,6 +27,8 @@ namespace CompanySite
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Подключение config-файла из appsettings.json
+            Configuration.Bind("Project", new Config());
             //Порядок подключения сервисов очень важен!!!
             //1.Вывод сообщения об ошибках при их наличии
             if (env.IsDevelopment())
