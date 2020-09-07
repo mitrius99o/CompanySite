2644efed3ef47af23b377c2adca624d34fd199b1
mitrius99 <mitrius99o@gmail.com>
2020-09-07 18:57:11 +0300

    Created classes for entities of the project.Added connection string to db.

---
+++ CompanySite/Domain/Entities/EntityBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanySite.Domain.Entities
+{
+    public abstract class EntityBase//базовый класс для всех сущностей сайта
+    {
+        public EntityBase() => DateAdded = DateTime.UtcNow;
+        [Required]
+        public Guid Id { get; set; }//первичный ключ
+
+        [Display(Name = "Название (заголовок)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Краткое описание")]
+        public virtual string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Титульная картинка")]
+        public virtual string TitleImagePath { get; set; }
+
+        [Display(Name = "SEO метатег Title")]
+        public virtual string MetaTitle { get; set; }
+
+        [Display(Name = "SEO метатег Description")]
+        public virtual string MetaDescription { get; set; }
+
+        [Display(Name = "SEO метатег Keywords")]
+        public virtual string MetaKeywords { get; set; }
+
+        [DataType(DataType.Time)]
+        public virtual DateTime DateAdded { get; set; }
+
+
+    }
+}
---
+++ CompanySite/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanySite.Domain.Entities
+{
+    public class ServiceItem:EntityBase
+    {
+        [Required(ErrorMessage ="Заполните название услуги")]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Название услуги")]
+        public override string Title { get; set; }
+        
+        [Display(Name = "Краткое описание услуги")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Cодержание страницы")]
+        public override string Text { get; set; } 
+    }
+}
