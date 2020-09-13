68f9955c2d98fba0a936887b6640a535c4f7fc89
mitrius99 <mitrius99o@gmail.com>
2020-09-14 00:27:53 +0300

    Created Datamanager and added new services to Sturtup.cs

---
+++ CompanySite/Domain/DataManager.cs
@@ -0,0 +1,19 @@
+﻿using CompanySite.Domain.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanySite.Domain
+{
+    public class DataManager//удобное управление данными через этот класс
+    {
+        public ITextFieldsRepositoriy TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+        public DataManager(ITextFieldsRepositoriy textFieldsRepositoriy, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepositoriy;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
--- CompanySite/Startup.cs
+++ CompanySite/Startup.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
+using CompanySite.Domain;
+using CompanySite.Domain.Repositories.Abstract;
+using CompanySite.Domain.Repositories.EntityFramework;
 using CompanySite.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +25,38 @@ namespace CompanySite
         public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            //Подключение config-файла из appsettings.json
+            Configuration.Bind("Project", new Config());
+
+            //Подключение функционала в качестве сервисов через связывание данных
+            services.AddTransient<ITextFieldsRepositoriy, EFTextFieldsRepository>();//привязка интерфейса к реализации
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //Подключение контекста базы данных
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //Настройка identity системы
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //Настройка authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "BsCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/accout/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
             //поддержка контроллеров и представлений и установка совместимости с asp.net core 3.0
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
@@ -27,8 +65,7 @@ namespace CompanySite
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //Подключение config-файла из appsettings.json
-            Configuration.Bind("Project", new Config());
+            
             //Порядок подключения сервисов очень важен!!!
             //1.Вывод сообщения об ошибках при их наличии
             if (env.IsDevelopment())
@@ -37,6 +74,11 @@ namespace CompanySite
             }
             //2.Настройка маршрутизации
             app.UseRouting();
+
+            //Подключение аунтентификации и авторизации только после UseRouting и перед UseEndpoints
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             //3.Подключениеподдержки файлов (css, html, js)
             app.UseStaticFiles();
